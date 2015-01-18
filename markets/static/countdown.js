4c8f7d6d08f97c9fbee63ea35eb7928c2b834f42
ixtreon <ixtreon@gmail.com>
2015-01-18 15:43:46 +0000

    Added a dirty js countdown timer to track challenge expiration in real-time.

---
+++ markets/static/countdown.js
@@ -0,0 +1,16 @@
+﻿function getTimeSpanString(start, end)
+{
+    if (end < start)
+        end = start;
+
+    var t_left = (end - start) / 1000;
+
+    var seconds = Math.floor(t_left % 60);
+    t_left = t_left / 60;
+    var minutes = Math.floor(t_left % 60);
+    t_left = t_left / 60;
+    var hours = Math.floor(t_left % 24);
+    t_left = t_left / 24;
+    var days = Math.floor(t_left);
+    return "" + days + " days, " + hours + ":" + minutes + ":" + seconds;
+}
